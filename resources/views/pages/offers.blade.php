41d1529b7731a58f8e26bbc407e50330a1a40bf8
Dwayne <dmoneymunro@gmail.com>
2020-06-21 10:00:20 -0400

    created create form and edit forms

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -1,14 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-  <div id="about-page">
+  <div id="offers-page">
     <div class="content-box">
       <div class="row">
-        <div class="col-md-6">
-          <h1>Offers Page</h1>
-        </div>
-        <div class="col-md-6">
-          {{-- <img src="/img/about-page-img.jpeg" class="img-fluid"> --}}
+        <div class="col-md-12">
+          <h1>Sign Up to Notifications for Deals</h1>
+          <form>
+            <div class="row">
+              <div class="col-md-6">
+                 <div class="form-group">
+                  <label for="firstnameinput">First Name</label>
+                  <input type="text" class="form-control" id="firstnameinput" name="fname" placeholder="name@example.com">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="lastnameinput">Last Name</label>
+                  <input type="text" class="form-control" id="lastnameinput" name="lname" placeholder="(222)-222-2222">
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                 <div class="form-group">
+                  <label for="emailinput">Email address</label>
+                  <input type="email" class="form-control" id="emailinput" name="email" placeholder="name@example.com">
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                  <label for="phoneinput">Phone Number</label>
+                  <input type="text" class="form-control" id="phoneinput" placeholder="(222)-222-2222">
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6"></div>
+              <div class="col-md-6"></div>
+            </div>
+            <div class="form-group">
+              <button type="button" class="btn btn-primary">Confirm</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- resources/views/pages/waitlist.blade.php
+++ resources/views/pages/waitlist.blade.php
@@ -1,14 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-  <div id="about-page">
+  <div id="waitlist-page">
     <div class="content-box">
       <div class="row">
         <div class="col-md-6">
-          <h1>Waitlist Page</h1>
+          <h1>Get On The List</h1>
+          <form>
+            <div class="form-group">
+              <label for="emailinput">Email address</label>
+              <input type="email" class="form-control" id="emailinput" name="email" placeholder="name@example.com">
+            </div>
+            <div class="form-group">
+              <label for="phoneinput">Phone Number</label>
+              <input type="text" class="form-control" id="phoneinput" name="phone" placeholder="(222)-222-2222">
+            </div>
+            <div class="form-group">
+              <label for="guestinput">How Many Guest?</label>
+              <select class="form-control" name="guest" id="guestinput">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="timeinput">What Time?</label>
+              <select class="form-control" name="time" id="timeinput">
+                <option value="6">6:00 PM</option>
+                <option value="7">7:00 PM</option>
+                <option value="8">8:00 PM</option>
+                <option value="9">9:00 PM</option>
+                <option value="10">10:00 PM</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <button type="button" class="btn btn-primary">Confirm</button>
+            </div>
+          </form>
         </div>
         <div class="col-md-6">
-          {{-- <img src="/img/about-page-img.jpeg" class="img-fluid"> --}}
+          <p>Please Note: This is not a reservation. You will be added to the current waitlist. You may have a short wait once you arrive while we prepare your table.</p>
         </div>
       </div>
     </div>
